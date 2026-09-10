a093dc2c9da07ae5fbeac742f73358f2805cf933
chulin152512 <mexiball.inc@gmail.com>
2026-09-10 16:33:18 -0700

    Add i18n.js with available languages

---
+++ i18n.js
@@ -0,0 +1,9 @@
+// i18n.js
+export const IDIOMAS_DISPONIBLES = [
+  { code: 'es', name: 'Español' },
+  { code: 'en', name: 'Inglés' },
+  { code: 'pt', name: 'Portugués' },
+  { code: 'fr', name: 'Francés' },
+  { code: 'de', name: 'Alemán' },
+  { code: 'ja', name: 'Japonés' }
+];
