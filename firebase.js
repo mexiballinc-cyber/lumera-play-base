ccd7ac4cc146546d26b72eeb0b75dd5cedff4cc5
chulin152512 <mexiball.inc@gmail.com>
2026-09-12 23:44:01 -0700

    Refactor firebase.js for improved structure and exports

--- firebase.js
+++ firebase.js
@@ -1,10 +1,9 @@
-// firebase.js - Inicialización Centralizada de Firebase v12.19.0
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBynw4cMM1Y2iY8zeX37WJBcT6aNQMqGQc",
+export const firebaseConfig = {
+  apiKey: "AiZaSyBynw4CMM1Y2iY8zeX37WJBcT6aNQMgQc",
   authDomain: "lumera-79254.firebaseapp.com",
   projectId: "lumera-79254",
   storageBucket: "lumera-79254.firebasestorage.app",
@@ -12,11 +11,10 @@ const firebaseConfig = {
   appId: "1:4621510860:web:fcd7cfa27a59f196943a69"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 
-// Asignación global para compatibilidad con módulos de la app
-window.auth = auth;
+// Inyección limpia al ámbito global
 window.db = db;
+window.auth = auth;s
