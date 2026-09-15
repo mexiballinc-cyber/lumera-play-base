bf45261da5b6a7b2dbd195ec058d065274cc52c8
chulin152512 <mexiball.inc@gmail.com>
2026-09-14 20:39:19 -0700

    Add multilingual dictionary for localization

---
+++ i18n.js
@@ -0,0 +1,9 @@
+export const dictionary = {
+  es: { home: "Inicio", series: "Series", movies: "Películas", kids: "Kids 🌈", play: "▶ Reproducir", search: "Buscar..." },
+  en: { home: "Home", series: "TV Shows", movies: "Movies", kids: "Kids 🌈", play: "▶ Play", search: "Search..." },
+  fr: { home: "Accueil", series: "Séries", movies: "Films", kids: "Kids 🌈", play: "▶ Regarder", search: "Rechercher..." },
+  de: { home: "Start", series: "Serien", movies: "Filme", kids: "Kids 🌈", play: "▶ Abspielen", search: "Suchen..." },
+  it: { home: "Inizio", series: "Serie TV", movies: "Film", kids: "Kids 🌈", play: "▶ Riproduci", search: "Cerca..." },
+  pt: { home: "Início", series: "Séries", movies: "Filmes", kids: "Kids 🌈", play: "▶ Assistir", search: "Buscar..." },
+  ja: { home: "ホーム", series: "シリーズ", movies: "映画", kids: "キッズ 🌈", play: "▶ 再生", search: "検索..." }
+};
